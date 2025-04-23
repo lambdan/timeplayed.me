4fb7924df8099740115c212f094e6f2447d4ed8d
djs <lambdan@icloud.com>
2025-04-23 23:37:17 +0200

    Remove old scripts

--- scripts/build-push-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-TAG="dev"
-echo "Building and pushing version $TAG ..."
-
-docker build -f ../Dockerfile --tag "playtime-tracker:latest" --platform linux/amd64,linux/arm64 ..
-docker tag playtime-tracker:latest "davidsilverlind/playtime-tracker:$TAG"
-docker push "davidsilverlind/playtime-tracker:$TAG"
