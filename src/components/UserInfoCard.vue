b9342d496d4bb833308bc5086e76951e5d5889d6
djs <lambdan@icloud.com>
2025-06-21 23:47:11 +0200

    User Info Card basics

---
+++ src/components/UserInfoCard.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import type { User } from "../models/models";
+
+const FALLBACK_AVATAR = "https://cdn.discordapp.com/embed/avatars/0.png";
+
+defineProps<{ user: User }>();
+</script>
+
+<template>
+  <div class="card">
+    <h1 class="card-header">{{ user.name }}</h1>
+    <div class="card-body">
+      <div class="row">
+        <div class="col-lg-2 mb-4">
+          <img
+            :src="user.avatar_url ?? FALLBACK_AVATAR"
+            class="img-thumbnail img-fluid rounded-circle"
+          />
+        </div>
+
+        <div class="col mb-4">
+          <table class="table table-responsive table-hover">
+            <tbody>
+              <tr>
+                <td><b>Discord ID:</b></td>
+                <td>{{ user.id }}</td>
+              </tr>
+              <tr>
+                <td><b>First session:</b></td>
+                <td>Mon, 20 Jan 2025 19:11:26 GMT</td>
+              </tr>
+              <tr>
+                <td><b>Last active:</b></td>
+                <td>Sat, 21 Jun 2025 12:09:58 GMT</td>
+              </tr>
+              <tr>
+                <td><b>Active days:</b></td>
+                <td>139</td>
+              </tr>
+              <tr>
+                <td><b>Longest streak:</b></td>
+                <td>31</td>
+              </tr>
+              <tr>
+                <td><b>Longest break:</b></td>
+                <td>3 days</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="col mb-4">
+          <table class="table table-responsive table-hover">
+            <tbody>
+              <tr>
+                <td><b>Games played:</b></td>
+                <td>48</td>
+              </tr>
+              <tr>
+                <td><b>Total playtime:</b></td>
+                <td>361.5 hours</td>
+              </tr>
+              <tr>
+                <td><b>Average playtime/game:</b></td>
+                <td>7.5 hours</td>
+              </tr>
+              <tr>
+                <td><b>Total sessions:</b></td>
+                <td><a href="/user/84733730387673088/sessions">451</a></td>
+              </tr>
+              <tr>
+                <td><b>Average sessions/game:</b></td>
+                <td>9</td>
+              </tr>
+              <tr>
+                <td><b>Average session length:</b></td>
+                <td>48 minutes</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
+import UserInfoCard from "../components/UserInfoCard.vue";
 import RecentActivityCard from "../components/RecentActivityCard.vue";
-import type { Activity, API_Activities } from "../models/models";
+import {
+  type User,
+  type Activity,
+  type API_Activities,
+} from "../models/models";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
 const activities = ref<Activity[]>([]);
+const user = ref<User>();
 
 onMounted(async () => {
   const userId = route.params.id as string;
@@ -15,9 +21,12 @@ onMounted(async () => {
     ...activity,
     createdAt: new Date(activity.timestamp),
   }));
+  const userRes = await fetch(`/api/users/${userId}`);
+  user.value = await userRes.json();
 });
 </script>
 
 <template>
+  <UserInfoCard v-if="user" :user="user" />
   <RecentActivityCard :activities="activities" />
 </template>
