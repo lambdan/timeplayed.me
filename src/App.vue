311c2393d55b4d0396590cf97bc0089cf1fe5ddd
djs <lambdan@icloud.com>
2025-06-29 00:37:04 +0200

    Dark mode works again

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
+import { onMounted, onBeforeUnmount } from "vue";
+
+function setThemeBySystemPreference() {
+  const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  document.documentElement.setAttribute(
+    "data-bs-theme",
+    isDark ? "dark" : "light"
+  );
+}
+
+onMounted(() => {
+  setThemeBySystemPreference();
+  // Listen for changes in system theme
+  const mq = window.matchMedia("(prefers-color-scheme: dark)");
+  const handler = () => setThemeBySystemPreference();
+  mq.addEventListener("change", handler);
+  onBeforeUnmount(() => {
+    mq.removeEventListener("change", handler);
+  });
+});
 </script>
 
 <template>
-  <div class="mb-2">
-    <Navbar />
-    <main class="">
-      <router-view />
-    </main>
-    <Footer class="mt-2"></Footer>
-  </div>
+  <Navbar />
+  <main class="">
+    <router-view />
+  </main>
+  <Footer class="mt-2"></Footer>
 </template>
