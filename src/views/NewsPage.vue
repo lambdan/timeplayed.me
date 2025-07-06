7431fa51e9ba3d9401815e6c0798a6e12eae4b33
djs <lambdan@icloud.com>
2025-07-06 15:19:13 +0200

    2025-07-06 news post

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -3,6 +3,34 @@
     <h1 class="card-header">News</h1>
 
     <div class="card-body text-start">
+      <strong>2025-07-06: Incident</strong>
+      <p>
+        Due to a series of mishaps, <b>sessions between approximately 2025-07-06 01:00-14:40 (Swedish time) may have been lost</b>. I say may have, because I don't know if anyone actually played anything during that time.
+        <br><br>
+        This was because I manually added some sessions I had played, but accidentally set the wrong game one entry. So I used the <code>!setgame</code> command to change it, but it turns out that command had a problem where it changed the game for ALL sessions! Yikes...
+        <br><br>
+        Fortunately, I am backing up the database daily. Unfortunately, it is done on midnight UTC, so the backup containing data for 2025-07-05 had not been done yet, so I had to resort to the backup from the night before.
+        <br><br>
+        I then extracted the messed up database of 2025-07-05 (which had all the games set to one single game (<i>Serious Sam: Xbox</i> if you are curious)), and <i>manually</i> inserted the sessions of 2025-07-05 into the backup from the day before.
+        <br><br>
+        Fortunately, I had the most sessions that day, so I knew what games I had played, and I also visit the website frequently so I have a decent idea of what people are playing, so I could make some educated guesses on the others. If I guessed wrong, feel free to change it using the <code>!setgame</code> command, it should work now 🤞
+        <br>
+        Then I went to bed, thinking everything was fine.
+        <br><br>
+        The next day, while working on preventing stuff like this from happening again (more on that soon), I noticed my test sessions weren't saving. It turns out, because I had manually manipulated some sessions earlier, the sequence numbers in the database was messed up, and so the bot would not insert any new entries because the id's were conflicting. 
+        I actually had a similar problem earlier, when I was migrating from v1 of storage to v2, and I fixed that then by resetting the sequences after migration. But I very recently removed this part of the code, because the migration was done so I figured it wasn't needed anymore... 
+        Well anyway, the sequence resetting is back now, and everything is fine again.
+        <br><br>
+        To hopefully prevent something like this happening again, though you should not underestimate my stupidity, I've done a few things:
+        <ul>
+          <li>The database is now backed up hourly, instead of daily</li>
+          <li>I've set up additional logging. This way, if something screws up I can atleast manually go through the logs and recreate what is lost, should there be need to.</li>
+        </ul>
+        I could keep quiet about this, because most likely no one even noticed this happened, but I want to be transparent about incidents like this to build trust.
+        <br /><i>-djs</i>
+      </p>
+
+      <hr />
       <strong>2025-06-24: Version 2.0.0</strong>
       <p>
         The website has been completely redesigned! It now uses
