0725fd68bb2973844a7ae35508d050ac5d303c94
djs <lambdan@icloud.com>
2026-03-07 14:21:29 +0100

    Can set platform by fuzzymatching

--- backend/tpbackend/cmds/set_platform.py
+++ backend/tpbackend/cmds/set_platform.py
@@ -1,6 +1,7 @@
 from tpbackend.storage.storage_v2 import Platform, User
 from tpbackend.cmds.command import Command
 from tpbackend.storage.storage_v2 import Activity
+from tpbackend.utils import search_platforms
 
 
 class SetPlatformCommand(Command):
@@ -20,24 +21,45 @@ Example: set activities 123, 124 and 125 to platform 3: ```
 !set_platform 123,124,125 3 
 ```
 
+If you are lazy you can also *try* to use the name/abbreviation directly:
+```
+!set_platform 123 snes
+```
+But it will not work if there are multiple matches.
+
 Returns: Confirmation message
         """
         super().__init__(names=names, description=d, help=h)
 
     def execute(self, user: User, msg: str) -> str:
         splitted = msg.split(" ")
-        if len(splitted) != 2:
+        if len(splitted) < 2:
             return f"Invalid syntax. See `!help {self.names[0]}` for help."
         activities = splitted[0].split(",")
-        platform_id = splitted[1].strip()
-        return self.set_platform(user, activities, platform_id)
+        platform = None
+        try:
+            platform_id = splitted[1].strip()
+            platform = Platform.get_or_none(Platform.id == int(platform_id))  # type: ignore
+        except Exception:
+            # hmm maybe user did !set_platform 123 snes, try searching for it!
+            search_results = search_platforms(" ".join(splitted[1:]))
+            if len(search_results) == 1:
+                platform = search_results[0]
+            elif len(search_results) > 1:
+                msg = (
+                    "Not sure what platform you are referring to. Is it one of these?\n"
+                )
+                for p in search_results:
+                    msg += f"- **{p.id}** - {p.name or p.abbreviation}\n"  # type: ignore
+                msg += "If so, use the platform ID (the number) in the command"
+                return msg
+        if not platform:
+            return "Error: platform not found"
+        return self.set_platform(user, activities, platform)
 
     def set_platform(
-        self, user: User, activity_ids: list[str], platform_id: str
+        self, user: User, activity_ids: list[str], platform: Platform
     ) -> str:
-        platform = Platform.get_or_none(Platform.id == int(platform_id))  # type: ignore
-        if not platform:
-            return f"Error: Platform with id {platform_id} not found."
         msg = ""
         for activity_id in activity_ids:
             act = Activity.get_or_none(Activity.id == int(activity_id))  # type: ignore
