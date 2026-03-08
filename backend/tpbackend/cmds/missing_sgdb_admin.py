c0464494ad358afeeb963aa9bf0ec8357117892d
djs <lambdan@icloud.com>
2026-03-08 15:37:58 +0100

    Increment count, oops

--- backend/tpbackend/cmds/missing_sgdb_admin.py
+++ backend/tpbackend/cmds/missing_sgdb_admin.py
@@ -15,6 +15,7 @@ class MissingSGDBAdminCommand(AdminCommand):
         count = 0
         out = ""
         for game in missing:
+            count += 1
             out += f"- {game.name} (id: {game.id})\n"  # type: ignore
             if count > 50 or len(out) > 1337:
                 out += f"... and {missing.count() - count} more"
