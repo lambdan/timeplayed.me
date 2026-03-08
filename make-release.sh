28e47cf5891cc41b3c6fc3c21a21134d2d17dbb9
djs <lambdan@icloud.com>
2026-03-08 20:40:21 +0100

    Release script refinements

--- make-release.sh
+++ make-release.sh
@@ -3,12 +3,27 @@ set -e
 
 PACKAGE_JSON_PATH="./frontend/package.json"
 
+# check we are on correct branch
+CURRENT_BRANCH=$(git rev-parse --abrev-ref HEAD)
+if [ "$CURRENT_BRANCH" != "main" ]; then
+  echo "You are on branch $CURRENT_BRANCH - you can only release on main!"
+  exit 1
+fi
+
 # print current version from package.json
 CURRENT_VERSION=$(grep '"version":' $PACKAGE_JSON_PATH | sed -E 's/.*"version": "(.*)".*/\1/')
 echo "Current version (according to $PACKAGE_JSON_PATH): $CURRENT_VERSION"
 
 # ask for new version
 read -p "Enter new version (eg 1.2.3): " new_version
+
+# confirm
+read -p "You are about to release version $new_version. Are you sure? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "Aborting release."
+  exit 1
+fi
 
 # update version in package.json
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$new_version\"/" $PACKAGE_JSON_PATH
@@ -20,4 +35,14 @@ git commit -m "Release: $new_version"
 # create git tag
 git tag "$new_version"
 
-echo "OK!"
+echo "OK! Tagged $new_version"
+
+# push it?
+read -p "Do you want to push it? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  git push origin main --tags
+  echo "OK! Pushed to origin main and tags."
+else
+  echo "Not pushing. Remember to push your changes and tags!"
+fi
