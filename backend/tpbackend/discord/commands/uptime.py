1a36182ab8b77ba7b5a03f0849f2f2fd4ce4f363
djs <lambdan@icloud.com>
2026-06-13 21:20:12 +0200

    Tweak uptime command

--- backend/tpbackend/discord/commands/uptime.py
+++ backend/tpbackend/discord/commands/uptime.py
@@ -4,18 +4,18 @@ import datetime
 
 
 class UptimeCommand(AdminCommand):
-    started = 0
+    started = datetime.datetime.now()
 
     def __init__(self):
         super().__init__(["uptime"], "Bot uptime")
-        self.started = datetime.datetime.now().timestamp()
+        self.logger.info(f"Uptime command init. Started: {self.started}")
 
     def execute(self, user: User, msg: str) -> str:
-        now = datetime.datetime.now().timestamp()
-        uptime_seconds = int(now - self.started)
+        now = datetime.datetime.now()
+        uptime_seconds = int((now - self.started).total_seconds())
         days = uptime_seconds // 86400
         hours = (uptime_seconds % 86400) // 3600
         minutes = (uptime_seconds % 3600) // 60
         seconds = uptime_seconds % 60
-        uptime_str = f"{days}d {hours}h {minutes}m {seconds}s"
+        uptime_str = f"{days} days, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
         return f"Bot uptime: {uptime_str}"
